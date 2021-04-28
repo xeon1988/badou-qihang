10a190e1cb0be2cc7ab05a6ee29d2cf64221177a
zhangguifeng <173465221@qq.com>
2021-04-28 21:07:00 +0800

    Delete test/week2 directory

--- test/week2/test.py
+++
@@ -1,9 +0,0 @@
-total=0
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if ((i!=j)and(j!=k)and(k!=i)):
-                print(i,j,k)
-                total+=1
-print(total)
-
